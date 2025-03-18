f6edf0da0aab39d485f79b223f0cd66c087fa684
Láiza Bispo <laiza.bispo@aluno.ifsp.edu.br>
2025-03-18 10:24:38 -0300

    feat: add Album component

--- Etapa1/react-exemplos/src/components/Counter.jsx
+++ Etapa1/react-exemplos/src/components/Counter.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import '../App.css'
 
-function Counter(title, initial = 0) {
+
+function Counter({title, initial = 0}) {
   const [count, setCount] = useState(Number(initial))
+
 
   return (
     <>
-     <h1>{title}</h1> 
+     <h1>{title}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div> 
+      </div>
     </>
   )
 }
+
 
 export default Counter
