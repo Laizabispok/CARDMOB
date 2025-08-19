6b41fdde94f788520d7438b3f5655b9a9f4aec56
Láiza Bispo <LAIZA.BISPO@ALUNO.IFSP.EDU.BR>
2025-08-19 08:47:20 -0300

    Inclui novas telas: registro e login

--- Etapa3/loja-react-app/src/screens/HomeScreen.tsx
+++ Etapa3/loja-react-app/src/screens/HomeScreen.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import { useTheme } from '../contexts/ThemeContext'; //NOVA
+// EXCLUIR: import { useNavigation } from "@react-navigation/native";
+import { useTheme } from '../contexts/ThemeContext'; // Nova
 
-function HomeScreen() {
+function HomeScreen({ navigation }: any) { // INCLUIR {navigation}: any
+    // EXCLUIR const navigation = useNavigation();
     const { theme, toggleTheme } = useTheme();
     return (
         <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
             <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
                 Home Screen
             </Text>
-            <Button title="Alterar Tema" color={theme.colors.primary} 
-            onPress={toggleTheme}/>
-     </View>
+            <Button title="Alternar Tema" color={theme.colors.primary} onPress={toggleTheme}/>
+            <Button title="Ir para Detalhes" onPress={ () => navigation.navigate('Details')} />
+            <Button title="Login" onPress={ () => navigation.navigate('Login')}/>
+        </View>
     );
-
-
 }
 export default HomeScreen;
 
@@ -24,4 +25,4 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-})
+});
